2020b289dbf4b671bfca5b6f264f5ab878ab4d28
tmothes <tmothes@outlook.com>
2013-11-14 12:42:51 +0100

    Map prototype added
    A simple prototype for the map view avail. at localhost/map

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -7,7 +7,11 @@ import views.html.*;
 public class Application extends Controller {
 	
     public static Result index() {
-        return ok(index.render("Your new application is ready."));
+        return ok(index.render("Semantic Event Map"));
+    }
+    
+    public static Result displayMap() {
+    	return ok(map.render("Map"));
     }
     
 }
