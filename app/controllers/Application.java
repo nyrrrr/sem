41391ba89b1006a95a7454758a3a833160b3fa5a
tmothes <tmothes@outlook.com>
2013-11-26 19:14:49 +0100

    error correction and json example dummy

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,15 +1,22 @@
 package controllers;
 
-import java.util.Map;
+import java.io.File;
+import java.io.IOException;
+import java.io.Writer;
 
 import models.Request;
 import play.data.Form;
+import play.libs.Json;
 import play.mvc.Controller;
 import play.mvc.Result;
 import views.html.index;
 import views.html.map;
 
+import com.fasterxml.jackson.core.JsonGenerationException;
+import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 
 public class Application extends Controller {
 
@@ -37,8 +44,12 @@ public class Application extends Controller {
 		if (filledForm.hasErrors()) {
 			return controllers.Application.displayMap();
 		} else {
-			req = filledForm.get();
+			req = filledForm.get(); // queryType
 		}
-		return TODO;
+		//JsonNode v = Json.toJson(req);
+		//String jsonString = v.toString();
+		// TODO trigger some kind of request that will return a JSON
+		JsonNode result = Json.parse("{\"1\":1,\"2\" : {\"child\" : [1,2,3,4]} }");
+		return ok(result);
 	}
 }
