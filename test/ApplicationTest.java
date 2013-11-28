d71480b6c9350fd0b2609ceb118c3f0cf322a737
tmothes <tmothes@outlook.com>
2013-11-28 18:24:12 +0100

    minor changes
    gitignore and error removal

--- test/ApplicationTest.java
+++ test/ApplicationTest.java
@@ -4,17 +4,20 @@ import java.util.List;
 import java.util.Map;
 
 import com.fasterxml.jackson.databind.JsonNode;
+
+import models.Request;
+
 import org.junit.*;
 
 import play.mvc.*;
 import play.test.*;
 import play.data.DynamicForm;
+import play.data.Form;
 import play.data.validation.ValidationError;
 import play.data.validation.Constraints.RequiredValidator;
 import play.i18n.Lang;
 import play.libs.F;
 import play.libs.F.*;
-
 import static play.test.Helpers.*;
 import static org.fest.assertions.Assertions.*;
 
@@ -35,9 +38,9 @@ public class ApplicationTest {
 
     @Test
     public void renderTemplate() {
-        Content html = views.html.index.render("Your new application is ready.");
+        Content html = views.html.map.render("Your new application is ready.", Form.form(Request.class) );
         assertThat(contentType(html)).isEqualTo("text/html");
-        assertThat(contentAsString(html)).contains("Your new application is ready.");
+        assertThat(contentAsString(html)).contains("SEM");       
     }
 
 
